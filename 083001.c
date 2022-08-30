7014d5067a9aecac311ffd92eaeb04a1368ed004
dongyecanxin <112404293+dongyecanxin@users.noreply.github.com>
2022-08-30 21:52:12 +0800

    Add files via upload

---
+++ 083001.c
@@ -0,0 +1,149 @@
+#include<stdio.h>
+
+//int main()
+//{
+//		int i = 0;
+//		while (i <= 10)
+//		{
+//			i++;
+//			if (i == 5)
+//				continue;
+//			printf("%d ", i);
+//		}
+//
+//	//int i = 1;
+//	//while (i<=10)
+//	//{
+//	//	if (i == 5)
+//	//		break;
+//	//	printf("%d ",i);
+//	//	i++;
+//	//}
+//	return 0;
+//}
+
+//int main() 
+//{
+//	int n = 1;
+//	int m = 2;
+//	switch(n)
+//	{
+//	case 1:
+//		m++;
+//	case 2:
+//		n++;
+//	case 3:
+//		switch(n)
+//		{
+//		case 1:
+//			n++;
+//		case 2:
+//			m++;
+//			n++;
+//			break;
+//		}
+//	case 4:
+//		m++;
+//		break;
+//	default:
+//		break;
+//	}
+//	printf("m = %d,n = %d\n", m, n);
+//	return 0;
+//}//m=5,n=3
+
+//int main()
+//{
+//	int day = 0;
+//	scanf_s("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//	case 2:
+//	case 3:
+//	case 4:
+//	case 5:
+//		printf("工作日\n");
+//		break;
+//	case 6:
+//	case 7:
+//		printf("休息日\n");
+//		break;
+//	default :
+//		printf("输入错误\n");
+//		break;
+//	}
+
+	//switch (day)
+	//{
+	//case 1:
+	//	printf("xq1\n");
+	//	break;
+	//case 2:
+	//	printf("xq2\n");
+	//	break;
+	//case 3:
+	//	printf("xq3\n");
+	//	break;
+	//case 4:
+	//	printf("xq4\n");
+	//	break;
+	//case 5:
+	//	printf("xq5\n");
+	//	break;
+	//case 6:
+	//	printf("xq6\n");
+	//	break;
+	//case 7:
+	//	printf("xq7\n");
+	//	break;
+
+	//}
+
+	//if (1 == day)
+	//	printf("xq1\n");
+	//else if (2 == day)
+	//	printf("xq2\n");
+	//else if (3 == day)
+	//	printf("xq3\n");
+	//else if (4 == day)
+	//	printf("xq4\n");
+	//else if (5 == day)
+	//	printf("xq5\n");
+	//else if (6 == day)
+	//	printf("xq6\n");
+	//else if (7 == day)
+	//	printf("xq7\n");
+
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	while (i < 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 0;
+//	scanf_s("%d",&age);
+//	if (age < 12)
+//		printf("幼年\n");
+//	else if (age >= 12 && age < 18)
+//		printf("青少年\n");
+//	else if (age >= 18 && age < 28)
+//		printf("成年\n");
+//	else if (age >= 28 && age < 50)
+//		printf("壮年\n");
+//	else if (age >= 50)
+//		printf("老年\n");
+//
+//	return 0;
+//}
