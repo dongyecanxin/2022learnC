9bb1c46f11f3b923e2fc4c8052f59686a2c9d1fd
dongyecanxin <112404293+dongyecanxin@users.noreply.github.com>
2022-08-30 14:33:58 +0800

    Add files via upload

---
+++ 0828.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	*pc = 'a';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	printf("%p\n", &a);
+//	*p = 20;
+//	printf("a=%d\n", a);
+//
+//	return 0;
+//}
+
+//int Add(int x,int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n",arr[4]);
+//	int sum = Add(arr[3], arr[5]);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int max = 0;
+//	scanf_s("%d%d", &a, &b);
+//	max = a > b ? a : b;
+//	printf("the bigger number is= %d\n", max);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = ~a;
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main() 
+//{
+//	int arr[] = { 1,2,3,4,5,6 };
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+//	return 0;
+//}
