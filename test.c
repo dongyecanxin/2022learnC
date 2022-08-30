33b2c798665bb22d144e1b0aea39d50fdc6606b5
dongyecanxin <112404293+dongyecanxin@users.noreply.github.com>
2022-08-30 14:36:27 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	int sum = 0;
+	scanf_s("%d%d", &num1, &num2);
+	sum = num1 + num2;
+	printf("sum=%d\n", sum);
+	return 0;
+}
+
+//int main()
+//{
+//	short age = 20;
+//	float weight = 95.6f;
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+	//char ch = 'A';
+	//printf("%c\n", ch);
+	//int age = 20;
+	//printf("%d", age);
+	//long num = 100;
+	//printf("%d\n", num);
+	//float f = 5.0;
+	//printf("%f\n", f);
+	//double d = 3.14;
+	//printf("%lf\n", d);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
