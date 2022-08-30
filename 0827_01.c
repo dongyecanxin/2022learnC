3b7a41250272ed8d2e8db28cb84d3b343e91e35b
dongyecanxin <112404293+dongyecanxin@users.noreply.github.com>
2022-08-30 14:29:49 +0800

    Add files via upload

---
+++ 0827_01.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	printf("%c\n", '\x61');
+	return 0;
+}
+
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\32\test.c"));
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%c\n", '\132');
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c','\0'};
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c','\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 4;
+//	printf("%d\n", num);
+//	return 0;
+//}
